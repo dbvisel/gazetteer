dc9630d33381e5f24aa8ba9d4b5b20ab1eae311e
Dan Visel <dbvisel@gmail.com>
2022-04-27 15:58:46 +0800

    initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Gazetteer`,
+    description: `Digital version of Robert F. Austin's Gazetteer of SEAsian Place Names.`,
+    author: `Dan Visel <dbvisel@gmail.com`,
+    siteUrl: `https://gazetteer.netlify.app/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,15 +20,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `gazetteer`,
+        short_name: `gazetteer`,
         start_url: `/`,
-        background_color: `#663399`,
+        background_color: `#880000`,
         // This will impact how browsers show your PWA/website
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/nus-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
